aef54c27ba5218985afae3e078b7c1ce539932c1
JackNguyenCSUF <38566988+JackNguyenCSUF@users.noreply.github.com>
2019-05-17 18:12:49 -0700

    Add files via upload
    gey

---
+++ Assets/NextLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider obj)
+
+    {
+        if (obj.name == "player")
+
+        { SceneManager.LoadScene("Level 2"); }
+
+    }
+}
